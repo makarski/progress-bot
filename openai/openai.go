65600cbb7a0da5d1dbe6dc4652c8293fcad08ff1
Simon Makarski <code@makarski.dev>
2025-02-17 23:35:36 +0100

    Strip markdown json from openai response

--- openai/openai.go
+++ openai/openai.go
@@ -3,6 +3,7 @@ package openai
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/sashabaranov/go-openai"
 )
@@ -11,18 +12,18 @@ import (
 const dailySystemPrompt = `Please generate a daily report that serves as a communication on behalf of the Engineering Team Lead. Your task is to read a JSON-serialized summary that outlines the team's progress over a period of time and to estimate the areas impacted by each issue or task.
 
 Instructions:
-	
+
 1. For each issue or task, try to identify the areas that it impacts (e.g., security, compliance, user experience, etc.).
 2. Format your response as a JSON object.
-3. Use the key 'issue_key' for each issue or task (e.g., "LI-607", "LI-617").
+3. Use the key 'issue_key' for each issue or task (e.g., "ABC-123", "ABC-456").
 4. For each issue or task, provide a brief description of the guessed impact as the value.
 Include a 'summary' key in the JSON object to provide an overall summary of the team's activities and the areas impacted.
 
 Example JSON Response:
 {
 	"summary": "Over the reported period, the team completed 3 tasks and resolved 1 bug. The areas impacted are: security, compliance, user access, and permissions.",
-	"LI-607": "This task involved managing roles in the identity management system and primarily impacts the areas of user access and permissions.",
-	"LI-617": "This task focused on tracking login activities, affecting the system's security and compliance."
+	"ABC-123": "This task involved managing roles in the identity management system and primarily impacts the areas of user access and permissions.",
+	"ABC-456": "This task focused on tracking login activities, affecting the system's security and compliance."
 }`
 
 // OpenAI is a wrapper around the OpenAI API.
@@ -66,5 +67,9 @@ func (oai *OpenAI) CompileReport(ctx context.Context, jsonb []byte) (string, err
 		return "", fmt.Errorf("failed to create chat completion: %s", err)
 	}
 
-	return resp.Choices[0].Message.Content, nil
+	res := resp.Choices[0].Message.Content
+	res = strings.TrimLeft(res, "```json")
+	res = strings.TrimRight(res, "```")
+
+	return res, nil
 }
